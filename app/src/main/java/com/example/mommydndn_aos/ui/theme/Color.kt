28c9176162bc924eef27b4e9ff8e7dc58bde1ea4
honggi123 <89631493+honggi123@users.noreply.github.com>
2023-08-28 21:01:08 +0900

    테마 color, typography, shape, paddings 기본 설정

--- app/src/main/java/com/example/mommydndn_aos/ui/theme/Color.kt
+++ app/src/main/java/com/example/mommydndn_aos/ui/theme/Color.kt
@@ -1,8 +1,55 @@
 package com.example.mommydndn_aos.ui.theme
 
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
 import androidx.compose.ui.graphics.Color
 
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+val White = Color(0xFFFFFFFF)
+
+val Grey50 = Color(0xFF9F9FA)
+val Grey100 = Color(0xFFF0F2F4)
+val Grey200 = Color(0xFFE1E3E8)
+val Grey300 = Color(0xFFCBD0D6)
+val Grey400 = Color(0xFFA7AFB9)
+val Grey500 = Color(0xFF7F8A96)
+val Grey600 = Color(0xFF606A78)
+val Grey700 = Color(0xFF454E5D)
+val Grey800 = Color(0xFF2D3541)
+val Grey900 = Color(0xFF181C23)
+val Grey1000 = Color(0xFF0E1117)
+
+val Salmon50 = Color(0xFF9F9FA)
+val Salmon100 = Color(0xFFF0F2F4)
+val Salomon200 = Color(0xFFE1E3E8)
+val Salmon300 = Color(0xFFCBD0D6)
+val Salmon400 = Color(0xFFA7AFB9)
+val Salmon500 = Color(0xFF7F8A96)
+val Salmon600 = Color(0xFF606A78)
+val Salmon700 = Color(0xFF454E5D)
+val Salmon800 = Color(0xFF2D3541)
+val Salmon900 = Color(0xFF181C23)
+val Salmon1000 = Color(0xFF0E1117)
+
+val GreyOpacity100 = Color(0x26606A78)
+val GreyOpacity200 = Color(0x40606A78)
+val GreyOpacity300 = Color(0x59454E5D)
+val GreyOpacity400 = Color(0x73454E5D)
+val GreyOpacity500 = Color(0x8C2D3541)
+val GreyOpacity600 = Color(0xA62D3541)
+val GreyOpacity700 = Color(0xBF2D3541)
+val GreyOpacity800 = Color(0xD9181C23)
+val GreyOpacity900 = Color(0xF2181C23)
+
+val WhiteOpacity100 = Color(0x26FFFFFF)
+val WhiteOpacity200 = Color(0x40FFFFFF)
+val WhiteOpacity300 = Color(0x59FFFFFF)
+val WhiteOpacity400 = Color(0x73FFFFFF)
+val WhiteOpacity500 = Color(0x8CFFFFFF)
+val WhiteOpacity600 = Color(0xA6FFFFFF)
+val WhiteOpacity700 = Color(0xBFFFFFFF)
+val WhiteOpacity800 = Color(0xD9FFFFFF)
+val WhiteOpacity900 = Color(0xF2FFFFFF)
--- app/src/main/java/com/example/mommydndn_aos/ui/theme/Mommydndn.kt
+++ app/src/main/java/com/example/mommydndn_aos/ui/theme/Mommydndn.kt
@@ -6,13 +6,13 @@ import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
+private val DarkColors = darkColors(
     primary = Purple200,
     primaryVariant = Purple700,
     secondary = Teal200
 )
 
-private val LightColorPalette = lightColors(
+private val LightColors = lightColors(
     primary = Purple500,
     primaryVariant = Purple700,
     secondary = Teal200
@@ -30,14 +30,14 @@ private val LightColorPalette = lightColors(
 @Composable
 fun MommydndnaosTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
-        DarkColorPalette
+        DarkColors
     } else {
-        LightColorPalette
+        LightColors
     }
 
     MaterialTheme(
         colors = colors,
-        typography = Typography,
+        typography = MommydndnTypography,
         shapes = Shapes,
         content = content
     )
