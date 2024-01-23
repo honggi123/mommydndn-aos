754549518c463228cda49b70d2ed263c0e8c1bae
space0clouds <space0clouds@gmail.com>
2024-01-24 07:39:54 +0900

    manual_merge: develop

--- app/src/main/java/com/mommydndn/app/data/network/model/NetworkCareType.kt
+++ app/src/main/java/com/mommydndn/app/data/network/model/NetworkCareType.kt
@@ -5,8 +5,8 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 enum class NetworkCareType {
-    @SerialName("PARENTING") CHILD_CARE,
-    @SerialName("NURSING") SENIOR_CARE,
-    @SerialName("SCHOOL") SCHOOL_TRANSPORTATION,
-    @SerialName("HOUSEKEEPING") HOUSEKEEPING,
+    @SerialName("PARENTING") ChildCare,
+    @SerialName("NURSING") SeniorCare,
+    @SerialName("SCHOOL") SchoolTransportation,
+    @SerialName("HOUSEKEEPING") Housekeeping,
 }
