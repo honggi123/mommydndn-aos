eb9024a95409fe88ee57b6582d2de9785535818b
honggi <ghdrl7526@gmail.com>
2024-01-30 09:57:44 +0900

    feature: signin_repository

--- app/src/main/java/com/mommydndn/app/domain/repository/UserRepository.kt
+++ app/src/main/java/com/mommydndn/app/domain/repository/UserRepository.kt
@@ -1,6 +1,15 @@
 package com.mommydndn.app.domain.repository
 
+import com.mommydndn.app.domain.model.OAuthProvider
 
 interface UserRepository {
+    suspend fun signIn(
+        oauthProvider: OAuthProvider,
+        accessToken: String,
+        deviceToken: String
+    )
 
+    suspend fun getGoogleAccessToken(
+        authCode: String
+    ): String
 }
