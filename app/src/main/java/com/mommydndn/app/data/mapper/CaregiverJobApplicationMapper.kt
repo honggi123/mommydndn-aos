9bbce5ecce089c0fbb03c444587679454d66e072
honggi <ghdrl7526@gmail.com>
2024-02-01 17:08:26 +0900

    feature: home_repository

--- app/src/main/java/com/mommydndn/app/data/mapper/CaregiverJobApplicationMapper.kt
+++ app/src/main/java/com/mommydndn/app/data/mapper/CaregiverJobApplicationMapper.kt
@@ -1,2 +1,8 @@
 package com.mommydndn.app.data.mapper
 
+import com.mommydndn.app.data.network.model.NetworkNearbyCareWorker
+import com.mommydndn.app.domain.model.CaregiverJobApplication
+
+fun List<NetworkNearbyCareWorker>.transformToDomainCaregiverJobApplication() : List<CaregiverJobApplication> = map {
+    TODO()
+}
