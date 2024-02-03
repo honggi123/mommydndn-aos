e421c4f3887e68ede7f5a0f91a077ca5de3211f3
honggi123 <89631493+honggi123@users.noreply.github.com>
2024-02-03 22:47:44 +0900

    feature: home_usecase

--- app/src/main/java/com/mommydndn/app/domain/usecase/care/GetNearbyCaregiverJobApplicationsUseCase.kt
+++ app/src/main/java/com/mommydndn/app/domain/usecase/care/GetNearbyCaregiverJobApplicationsUseCase.kt
@@ -1,7 +1,9 @@
 package com.mommydndn.app.domain.usecase.care
 
 import com.mommydndn.app.di.IODispatcher
+import com.mommydndn.app.domain.model.CaregiverJobApplication
 import com.mommydndn.app.domain.repository.CaregiverJobApplicationRepository
+import com.mommydndn.app.domain.usecase.UseCase
 import kotlinx.coroutines.CoroutineDispatcher
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -10,6 +12,9 @@ import javax.inject.Singleton
 class GetNearbyCaregiverJobApplicationsUseCase @Inject constructor(
     @IODispatcher coroutineDispatcher: CoroutineDispatcher,
     private val repository: CaregiverJobApplicationRepository,
-) {
+) : UseCase<Unit, List<CaregiverJobApplication>>(coroutineDispatcher) {
 
+    override suspend fun execute(parameters: Unit): List<CaregiverJobApplication> {
+        return repository.getNearbytCaregiverJobApplication()
+    }
 }
--- app/src/main/java/com/mommydndn/app/domain/usecase/care/GetNearbyCaregiverJobPostingsUseCase.kt
+++ app/src/main/java/com/mommydndn/app/domain/usecase/care/GetNearbyCaregiverJobPostingsUseCase.kt
@@ -4,6 +4,7 @@ import com.mommydndn.app.di.IODispatcher
 import com.mommydndn.app.domain.model.CaregiverJobPosting
 import com.mommydndn.app.domain.repository.CaregiverJobPostingRepository
 import com.mommydndn.app.domain.usecase.FlowUseCase
+import com.mommydndn.app.domain.usecase.UseCase
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -13,9 +14,9 @@ import javax.inject.Singleton
 class GetNearbyCaregiverJobPostingsUseCase @Inject constructor(
     @IODispatcher coroutineDispatcher: CoroutineDispatcher,
     private val repository: CaregiverJobPostingRepository,
-) : FlowUseCase<Unit, List<CaregiverJobPosting>>(coroutineDispatcher) {
+) : UseCase<Unit, List<CaregiverJobPosting>>(coroutineDispatcher) {
 
-    override fun execute(parameters: Unit): Flow<List<CaregiverJobPosting>> {
-        TODO()
+    override suspend fun execute(parameters: Unit): List<CaregiverJobPosting> {
+        return repository.getNearbyCaregiverJobPostings()
     }
 }
