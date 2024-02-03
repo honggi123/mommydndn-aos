94de1e29e68ae9d04bcc6ea31514a861cd3ba620
honggi123 <89631493+honggi123@users.noreply.github.com>
2024-02-03 23:41:55 +0900

    feature: home repository

--- app/src/main/java/com/mommydndn/app/domain/repository/UserRepository.kt
+++ app/src/main/java/com/mommydndn/app/domain/repository/UserRepository.kt
@@ -1,5 +1,8 @@
 package com.mommydndn.app.domain.repository
 
+import com.mommydndn.app.domain.model.Notification
+
 interface UserRepository {
 
+    suspend fun updateNotificationsAllowed(notifications: List<Notification>)
 }
