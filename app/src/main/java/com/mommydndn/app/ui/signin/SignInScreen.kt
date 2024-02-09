d712e3fb511b953d55afa39a043f2d473bdfdb34
honggi123 <89631493+honggi123@users.noreply.github.com>
2024-02-10 07:34:01 +0900

    feature: signin ui

--- app/src/main/java/com/mommydndn/app/ui/signin/SignInScreen.kt
+++ app/src/main/java/com/mommydndn/app/ui/signin/SignInScreen.kt
@@ -7,27 +7,17 @@ import androidx.activity.compose.ManagedActivityResultLauncher
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -39,21 +29,16 @@ import com.kakao.sdk.common.model.ClientError
 import com.kakao.sdk.common.model.ClientErrorCause
 import com.kakao.sdk.user.UserApiClient
 import com.mommydndn.app.BuildConfig
-import com.mommydndn.app.R
 import com.mommydndn.app.domain.model.OAuthProvider
 import com.mommydndn.app.ui.signin.components.LogoWithIntroduction
 import com.mommydndn.app.ui.signin.components.SignInTopAppBar
 import com.mommydndn.app.ui.signin.components.SocialLogin
-import com.mommydndn.app.ui.theme.Grey500
 import com.mommydndn.app.ui.theme.MommydndnTheme
-import com.mommydndn.app.ui.theme.Salmon600
 import com.mommydndn.app.ui.theme.White
-import com.mommydndn.app.ui.theme.heading800
-import com.mommydndn.app.ui.theme.paragraph300
 import com.navercorp.nid.NaverIdLoginSDK
 import com.navercorp.nid.oauth.OAuthLoginCallback
 
-// TODO: sign_in -> sign_up 플로우 정리
+// TODO: sign_in -> sign_up 플로우 정리 + navigation
 @Composable
 internal fun SignInScreenRoute(
     onExploreClick: () -> Unit,
@@ -147,9 +132,17 @@ internal fun SignInScreen(
         },
         modifier = modifier
     ) { innerPadding ->
-        LogoWithIntroduction(
-            modifier = modifier.padding(innerPadding).fillMaxSize()
-        )
+        Column(
+            modifier = modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            LogoWithIntroduction(
+                modifier = modifier.fillMaxWidth()
+            )
+        }
     }
 }
 
